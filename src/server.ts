53061b5e2e34fd9869259d75d6e6bb921dc72d00
Marcelo Porto <marcelo.portos@gmail.com>
2020-08-08 17:51:09 -0300

    entendendo como funciona o request.body no post
    commit apenas para demonstrar mais claramente como funciona o request.body e como usa-lo, assim como a necessidade de usar o express.json para entender o que vem...

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json()); //inclui plugin do express que entende JSON
+
+//Corpo (Request Body): Dados p/ criar ou atualizar registro
+app.post('/users', (request, response) => {
+    console.log(request.body);
     const users = [
         { nome: 'Marcelo', age: 37 },
         { nome: 'Pedro', age: 6 }
     ];
+    
     return response.json(users);
     console.log('Acessou a rota!');
 });
